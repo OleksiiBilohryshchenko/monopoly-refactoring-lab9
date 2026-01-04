8ddb5249b832bbe12cac10351af2dfb1219cf9e2
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2026-01-03 21:40:10 -0600

    Build JavaFX game board layout in MainWindowFx

--- Monopoly/src/main/java/edu/sumdu/monopoly/gui/fx/MainWindowFx.java
+++ Monopoly/src/main/java/edu/sumdu/monopoly/gui/fx/MainWindowFx.java
@@ -2,17 +2,29 @@ package edu.sumdu.monopoly.gui.fx;
 
 import edu.sumdu.monopoly.*;
 
+import edu.sumdu.monopoly.gui.GameBoardUtil;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+import edu.sumdu.monopoly.GameBoard;
+
 public class MainWindowFx implements MonopolyGUI {
 
     private final Stage stage;
     private final BorderPane root = new BorderPane();
+    private final List<GUICellFx> guiCells = new ArrayList<>();
+
+    private final GridPane northGrid = new GridPane();
+    private final GridPane southGrid = new GridPane();
+    private final GridPane westGrid  = new GridPane();
+    private final GridPane eastGrid  = new GridPane();
 
     private PlayerPanelFx[] playerPanels;
+
 
     public MainWindowFx(Stage stage) {
         this.stage = stage;
@@ -173,5 +185,37 @@ public class MainWindowFx implements MonopolyGUI {
     public TradeDialog openTradeDialog() {
         return null;
     }
+
+    public void setupGameBoard(GameBoard board) {
+        var dimension = GameBoardUtil.calculateDimension(board.getCellNumber());
+
+        buildEdge(northGrid, GameBoardUtil.getNorthCells(board), true);
+        buildEdge(southGrid, GameBoardUtil.getSouthCells(board), true);
+        buildEdge(westGrid,  GameBoardUtil.getWestCells(board),  false);
+        buildEdge(eastGrid,  GameBoardUtil.getEastCells(board),  false);
+
+        root.setTop(northGrid);
+        root.setBottom(southGrid);
+        root.setLeft(westGrid);
+        root.setRight(eastGrid);
+
+        buildPlayerPanels();
+    }
+
+    private void buildEdge(GridPane grid, List<Cell> cells, boolean horizontal) {
+        grid.getChildren().clear();
+
+        for (int i = 0; i < cells.size(); i++) {
+            GUICellFx view = new GUICellFx(cells.get(i));
+            guiCells.add(view);
+
+            if (horizontal) {
+                grid.add(view, i, 0);
+            } else {
+                grid.add(view, 0, i);
+            }
+        }
+    }
+
 
 }
